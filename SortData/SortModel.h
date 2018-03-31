ce9b920b7754ade06bc20317c8352577fb531107
xushuanghui <841272293@qq.com>
2018-03-31 14:57:14 +0800

    change

---
+++ SortData/SortModel.h
@@ -0,0 +1,13 @@
+//
+//  SortModel.h
+//  SortData
+//
+//  Created by shuanghui on 2018/3/31.
+//  Copyright © 2018年 shuanghui xu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SortModel : NSObject
+
+@end
---
+++ SortData/SortModel.m
@@ -0,0 +1,13 @@
+//
+//  SortModel.m
+//  SortData
+//
+//  Created by shuanghui on 2018/3/31.
+//  Copyright © 2018年 shuanghui xu. All rights reserved.
+//
+
+#import "SortModel.h"
+
+@implementation SortModel
+
+@end
